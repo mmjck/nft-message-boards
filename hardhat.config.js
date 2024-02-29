63a9a3d3c26866e2aa37773124f520428cf4017a
mmjck <msantosjck@gmail.com>
2024-02-29 17:57:40 -0400

    update contract deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+      sepolia: {
+        url: process.env.STAGING_ALCHEMY_KEY,
+        accounts: [process.env.PRIVATE_KEY]
+      },
+  },
 };
