ddd8b99646b6f4288d18efccb9fe7ac973e7f9b0
mmjck <msantosjck@gmail.com>
2024-02-29 16:12:53 -0400

    another address calls wave

--- scripts/run.js
+++ scripts/run.js
@@ -15,6 +15,10 @@ const main = async () => {
     await waveTxn.wait();
 
     waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait()
+    waveCount  = waveContract.getTotalWaves();
 }
 
 main().catch((error) =>{
