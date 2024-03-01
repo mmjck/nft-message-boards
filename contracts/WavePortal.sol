4d3dc89a8f13aaf8b1146642cbb4367d79706d28
mmjck <msantosjck@gmail.com>
2024-02-29 22:20:10 -0400

    make contract payable

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,9 +7,31 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    function wave() public {
+    event newWave(address indexed from, uint256 timestamp, string message);
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+
+    Wave[] waves;
+
+
+    function wave(string memory _message) public payable {
         totalWaves += 1;
         console.log("%s deu tchauzinho!", msg.sender);
+        
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        
+        emit newWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(prizeAmount <= address(this).balance,
+                "Tentando sacar mais dinheiro que o contrato possui.");
+
+        (bool success, ) = (msg.sender).call{value: prizeAmount }("");
+        require(success, "Falhou em sacar dinheiro do contrato.");
     }
 
     function getTotalWaves() public view returns (uint256) {
@@ -17,7 +39,12 @@ contract WavePortal {
         return totalWaves;
     }
 
-    constructor() {
+    function getAllWaves()public view returns(Wave[] memory){
+        return waves;
+    }
+
+
+    constructor() payable {
         console.log("Hello, World!!!");
     }
 }
